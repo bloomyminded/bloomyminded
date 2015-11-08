f17452d9f5d31d4246f12e6e7722a3d2c0ef3300
Cody Roberts <codyjroberts@gmail.com>
2015-11-08 12:56:42 -0600

    update S3 region

--- config/environments/production.rb
+++ config/environments/production.rb
@@ -30,6 +30,7 @@ Rails.application.configure do
     :s3_credentials => {
       :bucket => ENV['S3_BUCKET_NAME'],
       :access_key_id => ENV['S3_ACCESS_KEY_ID'],
+      :s3_host_name => 's3-us-west-2.amazonaws.com',
       :secret_access_key => ENV['S3_SECRET_ACCESS_KEY']
     }
   }
