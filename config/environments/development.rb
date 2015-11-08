b9dac2907a085ac97e5918f5f39d8adecf0b4c68
Cody Roberts <codyjroberts@gmail.com>
2015-11-08 12:33:19 -0600

    attempt at fixing paperclip again

--- config/environments/development.rb
+++ config/environments/development.rb
@@ -4,12 +4,9 @@ Rails.application.configure do
   Paperclip.options[:command_path] = "/usr/bin/"
   config.paperclip_defaults = {
     :storage => :s3,
-    :s3_credentials => {
-      :bucket => ENV['S3_BUCKET_NAME'],
-      :access_key_id => ENV['AWS_ACCESS_KEY_ID'],
-      :secret_access_key => ENV['AWS_SECRET_ACCESS_KEY']
-    }
+    :bucket => ENV['AWS_BUCKET_NAME']
   }
+
   # In the development environment your application's code is reloaded on
   # every request. This slows down response time but is perfect for development
   # since you don't have to restart the web server when you make code changes.
